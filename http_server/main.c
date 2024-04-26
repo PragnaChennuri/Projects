de5c2a75bca9d7782880ed9a17b95d707e01e78d
Pragna Chennuri <pragna@college10-100-64-57-156.resnet.ucsc.edu>
2024-04-25 23:04:21 -0700

    worked on socket, and bind functions

--- http_server/main.c
+++ http_server/main.c
@@ -1,2 +1,21 @@
 #include <sys/socket.h>
-int n_s = socket(AF_INET, SOCK_STREAM, 0);
+#include <netinet/in.h>
+
+// the struct that stores the IP address and the port #
+struct sockaddr_in address = {
+        AF_INET,
+        0x901f, //opposite order for port # and socket API
+        0
+};
+
+void main(){
+    // AF_INET is the regular IPv4 Internet Protocol
+    // SOCK_STREAM a simple TCP socket
+    int n_s = socket(AF_INET, SOCK_STREAM, 0); //returns a file descriptor of the socket
+
+    // binding the socket to a specific IP address and a specific port number
+    // n_s is the socket descriptor we made from above
+    // address: pointer that points to the struct which contains the IP address and port #
+    // sizeof(address): size of the structure
+    int b = bind(n_s, &address, sizeof(address));
+}
